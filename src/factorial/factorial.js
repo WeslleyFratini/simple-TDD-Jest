70c0b67bc80edb4af77d812ddcee5c42cde47f83
WeslleyFratini <weeslleey354@gmail.com>
2020-08-19 09:36:25 -0300

    terceiro teste

--- src/factorial/factorial.js
+++ src/factorial/factorial.js
@@ -1,10 +1,22 @@
-const getFactorial =  number => {
-  if(number ===1 ){
+import { typeErrorMessage, rangeErrorMessage } from "./utils";
 
+const getFactorial = (number) => {
+  const isNotNumber = typeof number !== "number";
+  const isNumberOutOfRange = number < 0 || number > 20;
+
+  if (isNotNumber) {
+    throw new TypeError(typeErrorMessage);
   }
-  return number * getFactorial(number -1)
-}
 
-  return factorial;
+  if (isNumberOutOfRange) {
+    throw new RangeError(rangeErrorMessage);
+  }
 
+  const isNumberOneorZero = number === 1 || number === 0;
 
+  return isNumberOneorZero ? 1 : number * getFactorial(number - 1);
+
+  return;
+};
+
+return factorial;
